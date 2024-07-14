8879cdfd3c8235771c6527007507943a82afb6e5
leonleerl <leon.runlongli@gmail.com>
2024-07-14 22:03:45 +0800

    redux async

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import { useDispatch, useSelector } from "react-redux";
 // 导入actionCreater
 import "./App.css";
-import { increment, decrement } from "./store/modules/counterStore";
+import { increment, decrement, addToNum } from "./store/modules/counterStore";
+import { fetchChannelList } from "./store/modules/channelStore";
+import { useEffect } from "react";
 
 function App() {
   const { count } = useSelector((state) => state.counter);
+  const { channelList } = useSelector((state) => state.channel);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchChannelList());
+  }, [dispatch]);
   return (
     <div className="App">
       <button onClick={() => dispatch(decrement())}>-</button>
       {count}
       <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(addToNum(10))}>add to 10</button>
+      <button onClick={() => dispatch(addToNum(20))}>add to 20</button>
+      <br></br>
+      <ul>
+        {channelList.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
